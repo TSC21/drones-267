9f92e8a7fdf3d0205578aad266b43121b63dbfa3
Sunil Shah <git@ss21k.co.uk>
2014-04-29 13:19:40 -0700

    First merge of framegrabberCV.c

--- code/pose_estimator/PoseEstimator.cpp
+++ code/pose_estimator/PoseEstimator.cpp
@@ -24,8 +24,51 @@
 #include "ros/ros.h"
 #include "std_msgs/Float64MultiArray.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>              /* low-level i/o */
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+
+#include <linux/videodev2.h>
+
 #include <sstream>
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
+
 using namespace std;
+
+enum io_method {
+    IO_METHOD_READ,
+    IO_METHOD_MMAP,
+    IO_METHOD_USERPTR,
+};
+
+struct buffer {
+    void *start;
+    size_t length;
+};
+
+static char         *dev_name = "/dev/video0";
+static enum         io_method io = IO_METHOD_MMAP;
+static int          fd = -1;
+struct buffer       *buffers;
+static unsigned int n_buffers;
+static int          set_format;
+static unsigned int width = 640;
+static unsigned int height = 480;
+static unsigned int fps = 30;
+static unsigned int timeout = 1;
+static unsigned int timeouts_max = 1;
+
+/* Allowed formats: V4L2_PIX_FMT_YUYV, V4L2_PIX_FMT_MJPEG, V4L2_PIX_FMT_H264
+*  The default will not be used unless the width and/or height is specified
+*  but the user does not specify a pixel format */
+static unsigned int pixel_format = V4L2_PIX_FMT_MJPEG;
+
+ros::Publisher cornersPub;
+ros::Publisher simplePosePub;
 
 std_msgs::Float64MultiArray makeCornersMsg(Mat_<double> const imagePts)
 {
@@ -49,43 +92,388 @@ std_msgs::Float64MultiArray makeSimplePoseMsg(Mat_<double> const simplePose)
     return simplePoseMsg;
 }
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "PoseEstimator");
-    ros::NodeHandle node;
-    int const queueSize = 1000;
-    ros::Rate loopRate(4);  // publish messages at 4 Hz
+static void errno_exit(const char *s) {
+    fprintf(stderr, "%s error %d, %s\n", s, errno, strerror(errno));
+    exit(EXIT_FAILURE);
+}
 
-    // "corners" message: [x1, y1, ..., x24, y24] -- contains the image
-    // coordinates of the 24 corners, in the order described in the paper
-    ros::Publisher cornersPub = \
-        node.advertise<std_msgs::Float64MultiArray>("corners", queueSize);
+static int xioctl(int fh, int request, void *arg) {
+    int r;
 
-    // "simplePose" message: [x, y, z, yaw] -- contains the estimate
-    // of the camera pose w.r.t. the landing pad
-    ros::Publisher simplePosePub = \
-        node.advertise<std_msgs::Float64MultiArray>("simplePose", queueSize);
+    do {
+        r = ioctl(fh, request, arg);
+    } while (-1 == r && EINTR == errno);
 
-    VideoCapture capture(0);
-    if(!capture.isOpened()) {
-        cerr << "Device inaccessible. Damn!" << endl;
-        return 1;
+    return r;
+}
+
+static void close_device(void) {
+    if (-1 == close(fd))
+        errno_exit("close");
+
+    fd = -1;
+}
+
+static void open_device(void) {
+    struct stat st;
+
+    if (-1 == stat(dev_name, &st)) {
+        fprintf(stderr, "Cannot identify '%s': %d, %s\n",
+            dev_name, errno, strerror(errno));
+        exit(EXIT_FAILURE);
     }
 
-    Mat frame;
-    Mat_<double> imagePts;
-    Mat_<double> simplePose;
+    if (!S_ISCHR(st.st_mode)) {
+        fprintf(stderr, "%s is no device\n", dev_name);
+        exit(EXIT_FAILURE);
+    }
 
-    // Set exposure and focus manually.
-    // Seems to take effect after several frames.
-    capture >> frame;
-    system("v4l2-ctl -d 0 -c exposure_auto=1");
-    system("v4l2-ctl -d 0 -c exposure_absolute=180");
-    system("v4l2-ctl -d 0 -c focus_auto=0");
-    system("v4l2-ctl -d 0 -c focus_absolute=0");
+    fd = open(dev_name, O_RDWR /* required */ | O_NONBLOCK, 0);
 
-    while(ros::ok()) {
-        capture >> frame;
+    if (-1 == fd) {
+        fprintf(stderr, "Cannot open '%s': %d, %s\n",
+            dev_name, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void init_read(unsigned int buffer_size) {
+    buffers = (buffer*) calloc(1, sizeof (*buffers));
+
+    if (!buffers) {
+        fprintf(stderr, "Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    buffers[0].length = buffer_size;
+    buffers[0].start = malloc(buffer_size);
+
+    if (!buffers[0].start) {
+        fprintf(stderr, "Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void init_mmap(void) {
+    struct v4l2_requestbuffers req;
+
+    CLEAR(req);
+
+    req.count = 4;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+
+    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req)) {
+        if (EINVAL == errno) {
+            fprintf(stderr, "%s does not support "
+                "memory mapping\n", dev_name);
+            exit(EXIT_FAILURE);
+        } else {
+            errno_exit("VIDIOC_REQBUFS");
+        }
+    }
+
+    if (req.count < 2) {
+        fprintf(stderr, "Insufficient buffer memory on %s\n",
+            dev_name);
+        exit(EXIT_FAILURE);
+    }
+
+    buffers = (buffer*) calloc(req.count, sizeof (*buffers));
+
+    if (!buffers) {
+        fprintf(stderr, "Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (n_buffers = 0; n_buffers < req.count; ++n_buffers) {
+        struct v4l2_buffer buf;
+
+        CLEAR(buf);
+
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = n_buffers;
+
+        if (-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf))
+            errno_exit("VIDIOC_QUERYBUF");
+
+        buffers[n_buffers].length = buf.length;
+        buffers[n_buffers].start =
+            mmap(NULL /* start anywhere */,
+            buf.length,
+            PROT_READ | PROT_WRITE /* required */,
+            MAP_SHARED /* recommended */,
+            fd, buf.m.offset);
+
+        if (MAP_FAILED == buffers[n_buffers].start)
+            errno_exit("mmap");
+    }
+}
+
+static void init_userp(unsigned int buffer_size) {
+    struct v4l2_requestbuffers req;
+
+    CLEAR(req);
+
+    req.count = 4;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_USERPTR;
+
+    if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req)) {
+        if (EINVAL == errno) {
+            fprintf(stderr, "%s does not support "
+                "user pointer i/o\n", dev_name);
+            exit(EXIT_FAILURE);
+        } else {
+            errno_exit("VIDIOC_REQBUFS");
+        }
+    }
+
+    buffers = (buffer*) calloc(4, sizeof (*buffers));
+
+    if (!buffers) {
+        fprintf(stderr, "Out of memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (n_buffers = 0; n_buffers < 4; ++n_buffers) {
+        buffers[n_buffers].length = buffer_size;
+        buffers[n_buffers].start = malloc(buffer_size);
+
+        if (!buffers[n_buffers].start) {
+            fprintf(stderr, "Out of memory\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
+static void init_device(void) {
+    struct v4l2_capability cap;
+    struct v4l2_cropcap cropcap;
+    struct v4l2_crop crop;
+    struct v4l2_format fmt;
+    struct v4l2_streamparm frameint;
+    unsigned int min;
+
+    if (-1 == xioctl(fd, VIDIOC_QUERYCAP, &cap)) {
+        if (EINVAL == errno) {
+            fprintf(stderr, "%s is no V4L2 device\n",
+                dev_name);
+            exit(EXIT_FAILURE);
+        } else {
+            errno_exit("VIDIOC_QUERYCAP");
+        }
+    }
+
+    if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
+        fprintf(stderr, "%s is no video capture device\n",
+            dev_name);
+        exit(EXIT_FAILURE);
+    }
+
+    switch (io) {
+    case IO_METHOD_READ:
+        if (!(cap.capabilities & V4L2_CAP_READWRITE)) {
+            fprintf(stderr, "%s does not support read i/o\n",
+                dev_name);
+            exit(EXIT_FAILURE);
+        }
+        break;
+
+    case IO_METHOD_MMAP:
+    case IO_METHOD_USERPTR:
+        if (!(cap.capabilities & V4L2_CAP_STREAMING)) {
+            fprintf(stderr, "%s does not support streaming i/o\n",
+                dev_name);
+            exit(EXIT_FAILURE);
+        }
+        break;
+    }
+
+
+    /* Select video input, video standard and tune here. */
+
+
+    CLEAR(cropcap);
+
+    cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+    if (0 == xioctl(fd, VIDIOC_CROPCAP, &cropcap)) {
+        crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        crop.c = cropcap.defrect; /* reset to default */
+
+        if (-1 == xioctl(fd, VIDIOC_S_CROP, &crop)) {
+            switch (errno) {
+            case EINVAL:
+                /* Cropping not supported. */
+                break;
+            default:
+                /* Errors ignored. */
+                break;
+            }
+        }
+    } else {
+        /* Errors ignored. */
+    }
+
+
+    CLEAR(fmt);
+
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (set_format) {
+        fmt.fmt.pix.width = width;
+        fmt.fmt.pix.height = height;
+        fmt.fmt.pix.pixelformat = pixel_format;
+        fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
+
+        if (-1 == xioctl(fd, VIDIOC_S_FMT, &fmt))
+            errno_exit("VIDIOC_S_FMT");
+
+        if (fmt.fmt.pix.pixelformat != pixel_format) {
+                fprintf(stderr,"Libv4l didn't accept pixel format. Can't proceed.\n");
+                exit(EXIT_FAILURE);
+        }
+
+        /* Note VIDIOC_S_FMT may change width and height. */
+    } else {
+        /* Preserve original settings as set by v4l2-ctl for example */
+        if (-1 == xioctl(fd, VIDIOC_G_FMT, &fmt))
+            errno_exit("VIDIOC_G_FMT");
+    }
+
+    CLEAR(frameint);
+
+    /* Attempt to set the frame interval. */
+    frameint.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    frameint.parm.capture.timeperframe.numerator = 1;
+    frameint.parm.capture.timeperframe.denominator = fps;
+    if (-1 == xioctl(fd, VIDIOC_S_PARM, &frameint))
+        fprintf(stderr, "Unable to set frame interval.\n");
+
+    /* Buggy driver paranoia. */
+    min = fmt.fmt.pix.width * 2;
+    if (fmt.fmt.pix.bytesperline < min)
+        fmt.fmt.pix.bytesperline = min;
+    min = fmt.fmt.pix.bytesperline * fmt.fmt.pix.height;
+    if (fmt.fmt.pix.sizeimage < min)
+        fmt.fmt.pix.sizeimage = min;
+
+    switch (io) {
+    case IO_METHOD_READ:
+        init_read(fmt.fmt.pix.sizeimage);
+        break;
+
+    case IO_METHOD_MMAP:
+        init_mmap();
+        break;
+
+    case IO_METHOD_USERPTR:
+        init_userp(fmt.fmt.pix.sizeimage);
+        break;
+    }
+}
+
+static void start_capturing(void) {
+    unsigned int i;
+    enum v4l2_buf_type type;
+
+    switch (io) {
+    case IO_METHOD_READ:
+        /* Nothing to do. */
+        break;
+
+    case IO_METHOD_MMAP:
+        for (i = 0; i < n_buffers; ++i) {
+            struct v4l2_buffer buf;
+
+            CLEAR(buf);
+            buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            buf.memory = V4L2_MEMORY_MMAP;
+            buf.index = i;
+
+            if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+                errno_exit("VIDIOC_QBUF");
+        }
+        type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        if (-1 == xioctl(fd, VIDIOC_STREAMON, &type))
+            errno_exit("VIDIOC_STREAMON");
+        break;
+
+    case IO_METHOD_USERPTR:
+        for (i = 0; i < n_buffers; ++i) {
+            struct v4l2_buffer buf;
+
+            CLEAR(buf);
+            buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+            buf.memory = V4L2_MEMORY_USERPTR;
+            buf.index = i;
+            buf.m.userptr = (unsigned long) buffers[i].start;
+            buf.length = buffers[i].length;
+
+            if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+                errno_exit("VIDIOC_QBUF");
+        }
+        type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        if (-1 == xioctl(fd, VIDIOC_STREAMON, &type))
+            errno_exit("VIDIOC_STREAMON");
+        break;
+    }
+}
+
+static void stop_capturing(void) {
+    enum v4l2_buf_type type;
+
+    switch (io) {
+    case IO_METHOD_READ:
+        /* Nothing to do. */
+        break;
+
+    case IO_METHOD_MMAP:
+    case IO_METHOD_USERPTR:
+        type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        if (-1 == xioctl(fd, VIDIOC_STREAMOFF, &type))
+            errno_exit("VIDIOC_STREAMOFF");
+        break;
+    }
+}
+
+
+static void uninit_device(void) {
+    unsigned int i;
+
+    switch (io) {
+    case IO_METHOD_READ:
+        free(buffers[0].start);
+        break;
+
+    case IO_METHOD_MMAP:
+        for (i = 0; i < n_buffers; ++i)
+            if (-1 == munmap(buffers[i].start, buffers[i].length))
+                errno_exit("munmap");
+        break;
+
+    case IO_METHOD_USERPTR:
+        for (i = 0; i < n_buffers; ++i)
+            free(buffers[i].start);
+        break;
+    }
+
+    free(buffers);
+}
+
+
+
+static void process_image(const void *p, int size) {
+
+        Mat frame;
+        IplImage * img;
+        Mat_<double> imagePts;
+        Mat_<double> simplePose;
+
+        frame = Mat(height, width, CV_8UC3, (void*)p);
+
         imagePts = detectCorners(frame);
         bool success = (imagePts.rows > 0);
         if(success) {
@@ -102,7 +490,192 @@ int main(int argc, char **argv)
             ROS_INFO("Could not detect all corners!");
         }
 
+
+        // // decode the image
+        // img = cvDecodeImage(&mat, 1);
+
+        // // release the image
+        // cvReleaseImage(&img);
+}    
+
+
+static int read_frame(void) {
+    struct v4l2_buffer buf;
+    unsigned int i;
+
+    switch (io) {
+    case IO_METHOD_READ:
+        if (-1 == read(fd, buffers[0].start, buffers[0].length)) {
+            switch (errno) {
+            case EAGAIN:
+                return 0;
+
+            case EIO:
+                /* Could ignore EIO, see spec. */
+
+                /* fall through */
+
+            default:
+                errno_exit("read");
+            }
+        }
+
+        process_image(buffers[0].start, buffers[0].length);
+        break;
+
+    case IO_METHOD_MMAP:
+        CLEAR(buf);
+
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+
+        if (-1 == xioctl(fd, VIDIOC_DQBUF, &buf)) {
+            switch (errno) {
+            case EAGAIN:
+                return 0;
+
+            case EIO:
+                /* Could ignore EIO, see spec. */
+
+                /* fall through */
+
+            default:
+                errno_exit("VIDIOC_DQBUF");
+            }
+        }
+
+        assert(buf.index < n_buffers);
+
+        process_image(buffers[buf.index].start, buf.bytesused);
+
+        if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+            errno_exit("VIDIOC_QBUF");
+        break;
+
+    case IO_METHOD_USERPTR:
+        CLEAR(buf);
+
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_USERPTR;
+
+        if (-1 == xioctl(fd, VIDIOC_DQBUF, &buf)) {
+            switch (errno) {
+            case EAGAIN:
+                return 0;
+
+            case EIO:
+                /* Could ignore EIO, see spec. */
+
+                /* fall through */
+
+            default:
+                errno_exit("VIDIOC_DQBUF");
+            }
+        }
+
+        for (i = 0; i < n_buffers; ++i)
+            if (buf.m.userptr == (unsigned long) buffers[i].start
+                && buf.length == buffers[i].length)
+                break;
+
+        assert(i < n_buffers);
+
+        process_image((void *) buf.m.userptr, buf.bytesused);
+
+        if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+            errno_exit("VIDIOC_QBUF");
+        break;
+    }
+
+    return 1;
+}
+
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "PoseEstimator");
+    ros::NodeHandle node;
+    int const queueSize = 1000;
+    ros::Rate loopRate(4);  // publish messages at 4 Hz
+
+    // "corners" message: [x1, y1, ..., x24, y24] -- contains the image
+    // coordinates of the 24 corners, in the order described in the paper
+    cornersPub = \
+        node.advertise<std_msgs::Float64MultiArray>("corners", queueSize);
+
+    // "simplePose" message: [x, y, z, yaw] -- contains the estimate
+    // of the camera pose w.r.t. the landing pad
+    simplePosePub = \
+        node.advertise<std_msgs::Float64MultiArray>("simplePose", queueSize);
+
+
+    open_device();
+    init_device();
+    start_capturing();
+
+
+    VideoCapture capture(0);
+    if(!capture.isOpened()) {
+        cerr << "Device inaccessible. Damn!" << endl;
+        return 1;
+    }
+
+    Mat frame;
+ 
+    unsigned int timeout_count = timeouts_max;
+
+    // Set exposure and focus manually.
+    // Seems to take effect after several frames.
+    capture >> frame;
+    system("v4l2-ctl -d 0 -c exposure_auto=1");
+    system("v4l2-ctl -d 0 -c exposure_absolute=180");
+    system("v4l2-ctl -d 0 -c focus_auto=0");
+    system("v4l2-ctl -d 0 -c focus_absolute=0");
+
+    while(ros::ok()) {
+
+
+            fd_set fds;
+            struct timeval tv;
+            int r;
+
+            FD_ZERO(&fds);
+            FD_SET(fd, &fds);
+
+            /* Timeout. */
+            tv.tv_sec = timeout;
+            tv.tv_usec = 0;
+
+            r = select(fd + 1, &fds, NULL, NULL, &tv);
+
+            if (-1 == r) {
+                if (EINTR == errno)
+                    continue;
+                errno_exit("select");
+            }
+
+            if (0 == r) {
+                if (timeout_count > 0) {
+                    timeout_count--;
+                } else {
+                    fprintf(stderr, "select timeout\n");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            if (read_frame())
+                break;
+            /* EAGAIN - continue select loop. */
+
+        // capture >> frame;
+        
+
         ros::spinOnce();
         loopRate.sleep();
     }
+
+    stop_capturing();
+    uninit_device();
+    close_device();
 }
