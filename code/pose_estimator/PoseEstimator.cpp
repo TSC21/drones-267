0f047ecb0348d4a3eebe66e84a7103706ee58fdc
Sunil Shah <git@ss21k.co.uk>
2014-05-01 14:30:51 -0700

    Compiles

--- code/pose_estimator/PoseEstimator.cpp
+++ code/pose_estimator/PoseEstimator.cpp
@@ -31,8 +31,12 @@ using namespace std;
 const int NUM_THREADS = 2;
 int idle_threads = NUM_THREADS;
 bool thread_busy [NUM_THREADS];
+bool thread_has_frame [NUM_THREADS];
 Mat thread_frames [NUM_THREADS];
 pthread_mutex_t idle_threads_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+ros::Publisher cornersPub;
+ros::Publisher simplePosePub;
 
 std_msgs::Float64MultiArray makeCornersMsg(Mat_<double> const imagePts)
 {
@@ -56,91 +60,26 @@ std_msgs::Float64MultiArray makeSimplePoseMsg(Mat_<double> const simplePose)
     return simplePoseMsg;
 }
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "PoseEstimator");
-    ros::NodeHandle node;
-    int const queueSize = 1000;
-    ros::Rate loopRate(4);  // publish messages at 4 Hz
-
-    // "corners" message: [x1, y1, ..., x24, y24] -- contains the image
-    // coordinates of the 24 corners, in the order described in the paper
-    ros::Publisher cornersPub = \
-        node.advertise<std_msgs::Float64MultiArray>("corners", queueSize);
-
-    // "simplePose" message: [x, y, z, yaw] -- contains the estimate
-    // of the camera pose w.r.t. the landing pad
-    ros::Publisher simplePosePub = \
-        node.advertise<std_msgs::Float64MultiArray>("simplePose", queueSize);
-
-    VideoCapture capture(0);
-    if(!capture.isOpened()) {
-        cerr << "Device inaccessible. Damn!" << endl;
-        return 1;
-    }
-
-    // Set exposure and focus manually.
-    // Seems to take effect after several frames.
-    capture >> frame;
-    system("v4l2-ctl -d 0 -c exposure_auto=1");
-    system("v4l2-ctl -d 0 -c exposure_absolute=180");
-    system("v4l2-ctl -d 0 -c focus_auto=0");
-    system("v4l2-ctl -d 0 -c focus_absolute=0");
-
-
-    // Create threads
-    pthread_t threads [NUM_THREADS];
-    
-    for (int t=0; t<NUM_THREADS; ++t) {
-        pthread_create ( &threads[t], NULL, processFrame, *cornersPub, *simplePosePub, *thread_id );
-    }
-
-    while(ros::ok()) {
-
-        Mat frame;
-        capture >> frame;
-        
-        // DESPATCH FRAME TO THREAD IF THERE ARE THREADS AVAILABLE
-        pthread_mutex_lock ( &idle_threads_mutex );
-        if (idle_threads) {
-            int idle_thread = 0;
-            while (idle_thread < NUM_THREADS && thread_busy[idle_thread]) {
-                idle_thread++;
-            }
-            thread_frames[idle_thread] = frame;             
-        }
-        pthread_mutex_unlock ( &idle_threads_mutex );
-
-        ros::spinOnce();
-        loopRate.sleep();
-    }
-
-    for (int t=0; t<NUM_THREADS; ++t) {
-        pthread_join ( &threads[t], NULL );
-    }
-}
-
-
-void setThreadBusy (thread_id) {
+void setThreadBusy (int thread_id) {
     pthread_mutex_lock ( &idle_threads_mutex );
     idle_threads--;
     thread_busy[thread_id] = true;
     pthread_mutex_unlock ( &idle_threads_mutex );
 }
 
-void setThreadIdle (thread_id) {
+void setThreadIdle (int thread_id) {
     pthread_mutex_lock ( &idle_threads_mutex );
     idle_threads++;
     thread_busy[thread_id] = false;
     pthread_mutex_unlock ( &idle_threads_mutex );
 }
 
-void *processFrame (ros::Publisher *cornersPub, ros::Publisher *simplePosePub, int *thread_id) {
+void *processFrame (void *arg) {
+    int thread_id = *(int *)arg;
     while (true)
     {
-        if (thread_frames[thread_id]) {
+        if (thread_has_frame[thread_id]) {
             setThreadBusy (thread_id);
-     
             Mat frame = thread_frames[thread_id];
             Mat_<double> imagePts;
             Mat_<double> simplePose;
@@ -160,13 +99,75 @@ void *processFrame (ros::Publisher *cornersPub, ros::Publisher *simplePosePub, i
                 ROS_INFO("Could not detect all corners!");
             }
 
-            frame.clear();
+            frame.release();
             setThreadIdle (thread_id);
         }
     }
+}
 
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "PoseEstimator");
+    ros::NodeHandle node;
+    int const queueSize = 1000;
+    ros::Rate loopRate(4);  // publish messages at 4 Hz
+
+    // "corners" message: [x1, y1, ..., x24, y24] -- contains the image
+    // coordinates of the 24 corners, in the order described in the paper
+    cornersPub = \
+        node.advertise<std_msgs::Float64MultiArray>("corners", queueSize);
+
+    // "simplePose" message: [x, y, z, yaw] -- contains the estimate
+    // of the camera pose w.r.t. the landing pad
+    simplePosePub = \
+        node.advertise<std_msgs::Float64MultiArray>("simplePose", queueSize);
+
+    VideoCapture capture(0);
+    if(!capture.isOpened()) {
+        cerr << "Device inaccessible. Damn!" << endl;
+        return 1;
+    }
+
+    // Set exposure and focus manually.
+    // Seems to take effect after several frames.
+    Mat temp_frame;
+    capture >> temp_frame;
+    temp_frame.release();
+    system("v4l2-ctl -d 0 -c exposure_auto=1");
+    system("v4l2-ctl -d 0 -c exposure_absolute=180");
+    system("v4l2-ctl -d 0 -c focus_auto=0");
+    system("v4l2-ctl -d 0 -c focus_absolute=0");
+
+
+    // Create threads
+    pthread_t threads [NUM_THREADS];
+    
+    for (int t=0; t<NUM_THREADS; ++t) {
+        pthread_create ( &threads[t], NULL, processFrame, (void *) &t);
+    }
+
+    while(ros::ok()) {
+
+        Mat frame;
+        capture >> frame;
         
+        // DESPATCH FRAME TO THREAD IF THERE ARE THREADS AVAILABLE
+        pthread_mutex_lock ( &idle_threads_mutex );
+        if (idle_threads) {
+            int idle_thread = 0;
+            while (idle_thread < NUM_THREADS && thread_busy[idle_thread]) {
+                idle_thread++;
+            }
+            thread_frames[idle_thread] = frame;
+            thread_has_frame[idle_thread] = true;
+        }
+        pthread_mutex_unlock ( &idle_threads_mutex );
 
+        ros::spinOnce();
+        loopRate.sleep();
+    }
 
-
+    for (int t=0; t<NUM_THREADS; ++t) {
+        pthread_join ( threads[t], NULL );
+    }
 }
